d94267f3f5da463402af29fdf9667ca508d37d6b
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-09-21 14:47:46 -0600

    init

---
+++ src/components/CalendarPics.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>tester tester tester</h1>
+  <form>
+    <input type="datetime-local" id="Test_DatetimeLocal">
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+export default {
+  setup() {
+    const state = reactive({
+      date: ''
+    })
+    return {
+      state
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/CalendarPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="calendar">
+    <div class="row my-3">
+      <div class="col-8">
+        <h1>This is the calander page</h1>
+        <CalendarPics />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarPage'
+}
+// your key to the api is this :  LUaAZ8ahtkNmFfCTlcAGc7O4pKbzgVUDf2tnQtCF
+</script>
